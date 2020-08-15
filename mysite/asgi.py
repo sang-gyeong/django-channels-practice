79d32d61822d1e0754f0b8bd8e665052e64ed7a3
이상경 <sknglee22@naver.com>
2020-08-16 02:03:24 +0900

    help

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,16 +1,9 @@
-# vi /usr/local/victolee/Channels_test/Channels_test_project/asgi.py
-"""
-ASGI entrypoint. Configures Django and then runs the application
-defined in the ASGI_APPLICATION setting.
-"""
+# myproject/asgi.py
 
 import os
 import django
 from channels.routing import get_default_application
-from whitenoise.django import DjangoWhiteNoise
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 django.setup()
-application = DjangoWhiteNoise(application)
-
+application = get_default_application()
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
